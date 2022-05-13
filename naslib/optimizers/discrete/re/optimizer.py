ea6e05c6679e8f6d9a248aa6e61c942eb7c7cb27
lukas <lukas.j.m.strack@gmail.com>
2022-05-13 10:20:45 +0200

    first test

--- naslib/optimizers/discrete/re/optimizer.py
+++ naslib/optimizers/discrete/re/optimizer.py
@@ -12,13 +12,13 @@ from naslib.utils.utils import AttrDict, count_parameters_in_MB
 from naslib.utils.logging import log_every_n_seconds
 
 logger = logging.getLogger(__name__)
-    
-        
+
+
 class RegularizedEvolution(MetaOptimizer):
-    
+
     # training the models is not implemented
     using_step_function = False
-    
+
     def __init__(self, config):
         super().__init__()
         self.config = config
@@ -38,20 +38,21 @@ class RegularizedEvolution(MetaOptimizer):
         self.search_space = search_space.clone()
         self.scope = scope if scope else search_space.OPTIMIZER_SCOPE
         self.dataset_api = dataset_api
-        
+
     def new_epoch(self, epoch):
-        # We sample as many architectures as we need 
-        if epoch < self.population_size:
+        raise NotImplementedError
+        # We sample as many architectures as we need
+        """if epoch < self.population_size:
             logger.info("Start sampling architectures to fill the population")
             # If there is no scope defined, let's use the search space default one
-            
+
             model = torch.nn.Module()   # hacky way to get arch and accuracy checkpointable
             model.arch = self.search_space.clone()
-            model.arch.sample_random_architecture(dataset_api=self.dataset_api)        
-            model.accuracy = model.arch.query(self.performance_metric, 
-                                              self.dataset, 
+            model.arch.sample_random_architecture(dataset_api=self.dataset_api)
+            model.accuracy = model.arch.query(self.performance_metric,
+                                              self.dataset,
                                               dataset_api=self.dataset_api)
-            
+
             self.population.append(model)
             self._update_history(model)
             log_every_n_seconds(logging.INFO, "Population size {}".format(len(self.population)))
@@ -60,19 +61,19 @@ class RegularizedEvolution(MetaOptimizer):
             while len(sample) < self.sample_size:
                 candidate = np.random.choice(list(self.population))
                 sample.append(candidate)
-            
+
             parent = max(sample, key=lambda x: x.accuracy)
 
             child = torch.nn.Module()   # hacky way to get arch and accuracy checkpointable
             child.arch = self.search_space.clone()
             child.arch.mutate(parent.arch, dataset_api=self.dataset_api)
-            child.accuracy = child.arch.query(self.performance_metric, 
-                                              self.dataset, 
+            child.accuracy = child.arch.query(self.performance_metric,
+                                              self.dataset,
                                               dataset_api=self.dataset_api)
 
             self.population.append(child)
-            self._update_history(child)
-        
+            self._update_history(child)"""
+
     def _update_history(self, child):
         if len(self.history) < 100:
             self.history.append(child)
@@ -98,15 +99,15 @@ class RegularizedEvolution(MetaOptimizer):
 
     def get_final_architecture(self):
         return max(self.history, key=lambda x: x.accuracy).arch
-    
-    
+
+
     def get_op_optimizer(self):
         raise NotImplementedError()
 
-    
+
     def get_checkpointables(self):
         return {'model': self.history}
-    
+
 
     def get_model_size(self):
         return count_parameters_in_MB(self.history)
