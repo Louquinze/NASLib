480eed518d366dc86d790a6de04bfea684e389b7
lukas <lukas.j.m.strack@gmail.com>
2022-12-09 11:26:20 +0100

    add new optimizer

--- naslib/optimizers/__init__.py
+++ naslib/optimizers/__init__.py
@@ -5,6 +5,7 @@ from .oneshot.darts_top_k_schedule_rev.optimizer import DARTSScheduledRevOptimiz
 from .oneshot.darts_top_k_schedule_rev_v2.optimizer import DARTSScheduledRevOptimizerV2
 from .oneshot.darts_top_k_schedule_rev_v3.optimizer import DARTSScheduledRevOptimizerV3
 from .oneshot.darts_top_k_schedule_rev_v4.optimizer import DARTSScheduledRevOptimizerV4
+from .oneshot.darts_top_k_schedule_rev_v5.optimizer import DARTSScheduledRevOptimizerV5
 from .oneshot.edge_popup.optimizer import EdgePopUpOptimizer
 from .oneshot.darts_prop.optimizer import DARTSPropOptimizer
 from .oneshot.gsparsity.optimizer import GSparseOptimizer
