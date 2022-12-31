66438e380fb5a23e657bca81dc3d1cdfdaae2609
lukas <lukas.j.m.strack@gmail.com>
2022-12-31 12:55:20 +0100

    add threshold feature

--- naslib/optimizers/oneshot/darts/optimizer.py
+++ naslib/optimizers/oneshot/darts/optimizer.py
@@ -174,7 +174,7 @@ class DARTSOptimizer(MetaOptimizer):
             # if train_loss.item() < best_model_loss:
             #     best_model_loss = train_loss.item()
             #     logger.info(f"Update best loss to: {best_model_loss}")
-            if train_loss.item() < 5 * best_model_loss:  # skipping bad arch selection of previous step
+            if train_loss.item() < 5 * best_model_loss and epoch > 4:  # skipping bad arch selection of previous step
                 train_loss.backward()
                 if self.grad_clip:
                     torch.nn.utils.clip_grad_norm_(self.graph.parameters(), self.grad_clip)
