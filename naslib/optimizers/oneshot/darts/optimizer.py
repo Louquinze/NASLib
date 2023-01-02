1583427df310beb57eb0e28f1b23faee016282c4
lukas <lukas.j.m.strack@gmail.com>
2023-01-02 12:27:06 +0100

    add threshold feature

--- naslib/optimizers/oneshot/darts/optimizer.py
+++ naslib/optimizers/oneshot/darts/optimizer.py
@@ -175,19 +175,22 @@ class DARTSOptimizer(MetaOptimizer):
             self.arch_optimizer.step()
 
             # Update op weights
-            self.op_optimizer.zero_grad()
-            logits_train = self.graph(input_train)
-            train_loss = self.loss(logits_train, target_train)
-            # if train_loss.item() < best_model_loss:
-            #     best_model_loss = train_loss.item()
-            #     logger.info(f"Update best loss to: {best_model_loss}")
-            if train_loss.item() < 5 * best_model_loss:  # skipping bad arch selection of previous step
-                train_loss.backward()
-                if self.grad_clip:
-                    torch.nn.utils.clip_grad_norm_(self.graph.parameters(), self.grad_clip)
-                self.op_optimizer.step()
-            else:
+            if epoch > 0:
                 self.op_optimizer.zero_grad()
+                logits_train = self.graph(input_train)
+                train_loss = self.loss(logits_train, target_train)
+                # if train_loss.item() < best_model_loss:
+                #     best_model_loss = train_loss.item()
+                #     logger.info(f"Update best loss to: {best_model_loss}")
+                if train_loss.item() < 5 * best_model_loss:  # skipping bad arch selection of previous step
+                    train_loss.backward()
+                    if self.grad_clip:
+                        torch.nn.utils.clip_grad_norm_(self.graph.parameters(), self.grad_clip)
+                    self.op_optimizer.step()
+                else:
+                    self.op_optimizer.zero_grad()
+            else:
+                logits_train, train_loss = logits_val, val_loss
 
         return logits_train, logits_val, train_loss, val_loss, best_model_loss
 
