d27a615e6644813d01ed3889f832f4a58ae50471
lukas <lukas.j.m.strack@gmail.com>
2023-01-03 20:46:10 +0100

    add threshold feature

--- naslib/optimizers/oneshot/edge_popup/optimizer.py
+++ naslib/optimizers/oneshot/edge_popup/optimizer.py
@@ -203,16 +203,10 @@ class EdgePopUpOptimizer(MetaOptimizer):
             self.op_optimizer.zero_grad()
             logits_train = self.graph(input_train)
             train_loss = self.loss(logits_train, target_train)
-            # if train_loss.item() < best_model_loss:
-            #     best_model_loss = train_loss.item()
-            #     logger.info(f"Update best loss to: {best_model_loss}")
-            if train_loss.item() < 5 * best_model_loss:  # skipping bad arch selection of previous step
-                train_loss.backward()
-                if self.grad_clip:
-                    torch.nn.utils.clip_grad_norm_(self.graph.parameters(), self.grad_clip)
-                self.op_optimizer.step()
-            else:
-                self.op_optimizer.zero_grad()
+            train_loss.backward()
+            if self.grad_clip:
+                torch.nn.utils.clip_grad_norm_(self.graph.parameters(), self.grad_clip)
+            self.op_optimizer.step()
 
         return logits_train, logits_val, train_loss, val_loss, best_model_loss
 
