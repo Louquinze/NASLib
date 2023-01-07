1070894feb315b1895ecc2e1ec56f97b18230764
lukas <lukas.j.m.strack@gmail.com>
2023-01-07 12:07:30 +0100

    add threshold feature

--- naslib/defaults/trainer.py
+++ naslib/defaults/trainer.py
@@ -267,7 +267,7 @@ class Trainer(object):
                         f"Merge saved tensors and cached tensors: {self.config.save_arch_weights_path}/tensor_{idx}.pt")
                     torch.save(x[idx], f'{self.config.save_arch_weights_path}/tensor_{idx}.pt')
 
-            if self.train_top1.avg > 35:
+            if self.train_top1.avg > 34:
                 logger.info(f"Early stopping")
                 self.periodic_checkpointer.step(self.epochs)
                 break
